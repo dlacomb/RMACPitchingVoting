4d11dcea471ff4f839bdf0a8804b743d044a0dc3
dlacomb <36368488+dlacomb@users.noreply.github.com>
2018-04-24 14:02:46 -0600

    Update Pitching Stats Complete Source Code.R

--- Pitching Stats Complete Source Code.R
+++ Pitching Stats Complete Source Code.R
@@ -1,5 +1,4 @@
-
-###Pitching Stats R Script
+> ###Pitching Stats R Script
 > library(readr)
 > Pitching_Stats <- read_csv("E:/Grad School/Practicum 2/Pitching Stats.csv", 
                              +     col_types = cols(`2b ` = col_number(), 
@@ -20,14 +19,15 @@
 > ps <- Pitching_Stats
 > summary(ps)
 > ps <- as.data.frame(ps)
+
 > ###Fix NA values through knn Imputation
-  > install.packages("DMwR")
+> install.packages("DMwR")
 > library(DMwR)
 > ps_complete <- knnImputation(ps[,4:28], k = 5, meth = "weighAvg")
 > View(ps_complete)
-> 
-  > ###Import Pitching Stats full dataset
-  > library(readr)
+
+> ###Import Pitching Stats Full Dataset
+> library(readr)
 > Pitching_Stats_Full_Dataset <- read_csv("E:/Grad School/Practicum 2/Pitching Stats Full Dataset.csv", 
                                           +     col_types = cols(`2b ` = col_number(), 
                                                                  +         `3b ` = col_number(), Award = col_number(), 
@@ -45,9 +45,9 @@
                                                                  +         `wp ` = col_number()), locale = locale(encoding = "ASCII"))
 > View(Pitching_Stats_Full_Dataset)
 > psfd <- Pitching_Stats_Full_Dataset
-> 
-  > ###Exploratory Data Analysis
-  > install.packages("dplyr")
+
+> ###Exploratory Data Analysis
+> install.packages("dplyr")
 > library(dplyr)
 > first <- filter(psfd, psfd$Award == 1)
 > second <- filter(psfd, psfd$Award == 2)
@@ -61,13 +61,15 @@
 > compareAwards <- rbind(AveStats_FirstTeam, AveStats_SecondTeam, AveStats_NoTeam)
 > barplot(compareAwards, beside = TRUE, xlab = "Pitching Metric", ylab = "Performance Value", main = "Mean Pitcher Statistics Comparison between 1st, 2nd, and No Team", col = c("gold", "grey", "brown"), cex.names = .5)
 > legend("topright", c("1st Team","2nd Team", "No Team"), pch=15, col=c("gold","grey", "brown"), bty="n")
-### Correlation plot between numeric variables
+
+> ###Correlation plot between numeric variables
 > install.packages("corrplot")
 > library(corrplot)
 > psfd_cor <- cor(psfd[,4:29])
 > corrplot(psfd_cor, method = "circle")
 > corrplot(psfd_cor, method = "number")
-###Balance Dataset with SMOTE Algorithm
+
+> ###Balance Dataset with SMOTE Algorithm
 > psfd$`Player ` <- as.factor(psfd$`Player `)
 > psfd$Position <- as.factor(psfd$Position)
 > psfd$Team <- as.factor(psfd$Team)
@@ -76,9 +78,10 @@
 > balanced_ps <- SMOTE(Award ~ ., psfd, perc.over = 650, perc.under = 2500)
 > summary(balanced_ps)
 > write.csv(balanced_ps, "Pitching Stats Balanced Data.csv")
-###Import Balanced Data Set
+
+> ###Import Balanced Data Set
 > library(readr)
-Pitching_Stats_Balanced_Data <- read_csv("E:/Grad School/Practicum 2/Pitching Stats Balanced Data.csv", 
+> Pitching_Stats_Balanced_Data <- read_csv("E:/Grad School/Practicum 2/Pitching Stats Balanced Data.csv", 
                                          +     col_types = cols(ConfStanding = col_number(), 
                                                                 +         Year = col_number(), ab = col_number(), 
                                                                 +         app = col_number(), bavg = col_number(), 
@@ -102,13 +105,14 @@ Pitching_Stats_Balanced_Data <- read_csv("E:/Grad School/Practicum 2/Pitching St
 > psbd <- as.data.frame(psbd)
 > summary(psbd)
 
-###Machine Learning Algorithms
-##Data Partitioning into Training and Test Sets
+> ###Machine Learning Algorithms
+> ##Data Partitioning into Training and Test Sets
 > ps_sets <- psbd[,2:29]
 > ind <- sample(2, nrow(ps_sets), replace = TRUE, prob = c(0.8,0.2))
 > trainps <- ps_sets[ind == 1,]
 > testps <- ps_sets[ind == 2,]
-#RandomForest
+
+> ##RandomForest
 > install.packages("randomForest")
 > library(randomForest)
 > fit <- randomForest(Award ~ ., data = trainps, method="class")
@@ -117,40 +121,44 @@ Pitching_Stats_Balanced_Data <- read_csv("E:/Grad School/Practicum 2/Pitching St
 > summary(fit)
 > importance(fit)
 > #Variable Importance Order
-  > # 1.) Strikeouts 2.) Wins 3.) Earned Run Average 4.) At Bats 5.) Innings Pitched 6.) Batting Average Against 7.) Team 8.) Runs 9.) Hits 10.) Doubles 11.) Earned Runs 12.) Appearances 13.) Combined Shutouts 14.) Walks 15.) Conference Standing 16.) Games Started 17.) Saves 18.) Complete Games 19.) Hit Batters 20.) Losses 21.) Year 22.) Homeruns 23.) Wild Pitches 24.) Triples 25.) Balks 26.) Individual Shutouts 27.) Position
-  > varImpPlot(fit)
+> # 1.) Strikeouts 2.) Wins 3.) Earned Run Average 4.) At Bats 5.) Innings Pitched 6.) Batting Average Against 7.) Team 8.) Runs 9.) Hits 10.) Doubles 11.) Earned Runs 12.) Appearances 13.) Combined Shutouts 14.) Walks 15.) Conference Standing 16.) Games Started 17.) Saves 18.) Complete Games 19.) Hit Batters 20.) Losses 21.) Year 22.) Homeruns 23.) Wild Pitches 24.) Triples 25.) Balks 26.) Individual Shutouts 27.) Position
+> varImpPlot(fit)
 > library(caret)
 > Prediction <- predict(fit, testps, type = "class")
 > confusionMatrix(Prediction, testps$Award)
 > #Accuracy = 98%
 
-  > ####KNN
-  > library(class)
+> ##KNN
+> library(class)
 > library(gmodels)
 > knnps <- psbd[,2:29]
 > knnps$Position <- as.numeric(knnps$Position)
 > knnps$Team <- as.numeric(knnps$Team)
 > #Normalize Data
-  > normalize <- function(x) {return ((x - min(x)) / (max(x) - min(x)))}
+> normalize <- function(x) {return ((x - min(x)) / (max(x) - min(x)))}
 > bps_n <- as.data.frame(lapply(knnps[1:27], normalize))
 > #Training and Testing Sets
-  > ind <- sample(2, nrow(bps_n), replace = TRUE, prob = c(0.8,0.2))
+> ind <- sample(2, nrow(bps_n), replace = TRUE, prob = c(0.8,0.2))
 > trainps <- bps_n[ind == 1,]
 > testps <- bps_n[ind == 2,]
 > trainpslabels <- knnps[ind == 1, 28]
 > testpslabels <- knnps[ind == 2, 28]
+> #K = 8
 > bps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 8)
 > CrossTable(x = testpslabels, y = bps_pred, prop.chisq = FALSE)
-  > #Accuracy = 78%
-  > bps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 5)
+> #Accuracy = 78%
+> #K = 5
+> bps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 5)
 > CrossTable(x = testpslabels, y = bps_pred, prop.chisq = FALSE)
-  > #Accuracy = 82%
-  > bps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 3)
+> #Accuracy = 82%
+> #K = 3
+> bps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 3)
 > CrossTable(x = testpslabels, y = bps_pred, prop.chisq = FALSE)
-  > #Accuracy = 87%
-  > bps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 2)
+> #Accuracy = 87%
+> #K = 2
+> bps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 2)
 > CrossTable(x = testpslabels, y = bps_pred, prop.chisq = FALSE)
-  > #Accuracy = 90%
+> #Accuracy = 90%
   
 > ###SVM
 > str(psbd)
@@ -159,23 +167,26 @@ Pitching_Stats_Balanced_Data <- read_csv("E:/Grad School/Practicum 2/Pitching St
 > pssvm <- psbd[,2:29]
 > library(e1071)
 > library(caret)
-> ### Training and Testing Set
+> #Training and Testing Set
 > ind <- sample(2, nrow(pssvm), replace = TRUE, prob = c(0.8,0.2))
 > trainps <- pssvm[ind == 1,]
 > testps <- pssvm[ind == 2,]
-> ### Regular SVM Model
+
+> #Regular SVM Model
 > svm.model <- svm(Award ~ ., data = trainps, kernal = "radial", cost = 1, gamma = 1/ncol(trainps), scale = FALSE)
 > summary(svm.model)
 > svm.pred <- predict(svm.model, testps)
 > confusionMatrix(svm.pred, testps$Award)
 > #Accuracy = 78%
-> ### Linear SVM Model
+
+> #Linear SVM Model
 > svm.model.linear <- svm(Award ~ ., data = trainps, kernal = "linear", cost = 1, scale = FALSE)
 > summary(svm.model.linear)
 > svm.pred.linear <- predict(svm.model.linear, testps)
 > confusionMatrix(svm.pred.linear, testps$Award)
 > #Accuracy = 78%
-> ### Polynomial SVM Model
+
+> #Polynomial SVM Model
 > svm.model.polynomial <- svm(Award ~ ., data = trainps, kernal = 'polynomial', cost = 1, degree = 3, coef0 = 0, gamma = 1/ncol(trainps), scale = FALSE)
 > summary(svm.model.polynomial)
 > svm.pred.polynomial <- predict(svm.model.polynomial, testps)
@@ -183,7 +194,7 @@ Pitching_Stats_Balanced_Data <- read_csv("E:/Grad School/Practicum 2/Pitching St
 > #Accuracy = 78%
   
 > ###Binomial Classification
-  > library(readr)
+> library(readr)
 > Pitching_Stats_Full_Dataset <- read_csv("E:/Grad School/Practicum 2/Pitching Stats Full Dataset.csv", 
                                           +  col_types = cols(`2b ` = col_number(), 
                                                               +  `3b ` = col_number(), Award = col_number(), 
@@ -206,6 +217,7 @@ Pitching_Stats_Balanced_Data <- read_csv("E:/Grad School/Practicum 2/Pitching St
 > psfd$Award <- as.factor(psfd$Award)
 > psfd <- as.data.frame(psfd)
 > psb <- psfd
+> ###Change Target Variable to Binomial Classification 
 > levels(psb$Award)
 [1] "0" "1" "2"
 > levels(psb$Award)[1] <- "No"
@@ -218,39 +230,43 @@ Pitching_Stats_Balanced_Data <- read_csv("E:/Grad School/Practicum 2/Pitching St
 > balanced_ps <- SMOTE(Award ~ ., psb, perc.over = 490, perc.under = 125)
 > summary(balanced_ps)
 > psbd <- balanced_ps
-> 
-  > #Binary Classification Model Analysis
-  > ####KNN
-  > library(class)
+
+> #Binary Classification Model Analysis
+> ###KNN
+> library(class)
 > library(gmodels)
 > str(psbd)
 > knnps <- psbd[,2:29]
 > knnps$Position <- as.numeric(knnps$Position)
 > knnps$Team <- as.numeric(knnps$Team)
 > #Normalize Data
-  > normalize <- function(x) {return ((x - min(x)) / (max(x) - min(x)))}
+> normalize <- function(x) {return ((x - min(x)) / (max(x) - min(x)))}
 > bps_n <- as.data.frame(lapply(knnps[1:27], normalize))
-  > #Training and Testing Sets
-  > ind <- sample(2, nrow(bps_n), replace = TRUE, prob = c(0.8,0.2))
+> #Training and Testing Sets
+> ind <- sample(2, nrow(bps_n), replace = TRUE, prob = c(0.8,0.2))
 > trainps <- bps_n[ind == 1,]
 > testps <- bps_n[ind == 2,]
 > trainpslabels <- knnps[ind == 1, 28]
 > testpslabels <- knnps[ind == 2, 28]
+> #K = 8
 > bps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 8)
 > CrossTable(x = testpslabels, y = bps_pred, prop.chisq = FALSE)
-  > #Accuracy = 95%
-  > ps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 5)
+> #Accuracy = 95%
+> #K = 5
+> ps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 5)
 > CrossTable(x = testpslabels, y = bps_pred, prop.chisq = FALSE)
-  > #Accuracy = 95%
-  > bps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 7)
+> #Accuracy = 95%
+> #K = 7
+> bps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 7)
 > CrossTable(x = testpslabels, y = bps_pred, prop.chisq = FALSE)
-  > #Accuracy = 96%
-  > bps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 6)
+> #Accuracy = 96%
+> #K = 6
+> bps_pred <- knn(train = trainps[,-1], test = testps[,-1], cl = trainpslabels, k = 6)
 > CrossTable(x = testpslabels, y = bps_pred, prop.chisq = FALSE)
-  > #Accuracy = 96%
+> #Accuracy = 96%
 
->  #Binomial Classification SVM
-  > psbd <- balanced_ps
+> #Binomial Classification SVM
+> psbd <- balanced_ps
 > str(psbd)
 > psbd$Team <- as.numeric(psbd$Team)
 > psbd$Position <- as.numeric(psbd$Position)
@@ -258,11 +274,11 @@ Pitching_Stats_Balanced_Data <- read_csv("E:/Grad School/Practicum 2/Pitching St
 > library(e1071)
 > library(caret)
 > ### Training and Testing Set
-  > ind <- sample(2, nrow(pssvm), replace = TRUE, prob = c(0.8,0.2))
+> ind <- sample(2, nrow(pssvm), replace = TRUE, prob = c(0.8,0.2))
 > trainps <- pssvm[ind == 1,]
 > testps <- pssvm[ind == 2,]
 > ### Regular SVM Model
-  > svm.model <- svm(Award ~ ., data = trainps, kernal = "radial", cost = 1, gamma = 1/ncol(trainps), scale = FALSE)
+> svm.model <- svm(Award ~ ., data = trainps, kernal = "radial", cost = 1, gamma = 1/ncol(trainps), scale = FALSE)
 > summary(svm.model)
 > svm.pred <- predict(svm.model, testps)
 > confusionMatrix(svm.pred, testps$Award)
@@ -274,7 +290,7 @@ Pitching_Stats_Balanced_Data <- read_csv("E:/Grad School/Practicum 2/Pitching St
 > confusionMatrix(svm.pred.linear, testps$Award)
 > #Accuracy = 80%
 > ### Polynomial SVM Model
-  > svm.model.polynomial <- svm(Award ~ ., data = trainps, kernal = 'polynomial', cost = 1, degree = 3, coef0 = 0, gamma = 1/ncol(trainps), scale = FALSE)
+> svm.model.polynomial <- svm(Award ~ ., data = trainps, kernal = 'polynomial', cost = 1, degree = 3, coef0 = 0, gamma = 1/ncol(trainps), scale = FALSE)
 > summary(svm.model.polynomial)
 > svm.pred.polynomial <- predict(svm.model.polynomial, testps)
 > confusionMatrix(svm.pred.polynomial, testps$Award)
